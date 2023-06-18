8ba32833a3fb9f08a7caf1633c098b86c8110d0c
harsha-0110 <harshavardhan.a2020@vitstudent.ac.in>
2023-06-18 22:07:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ColorPicker from './ColorPicker';
 
-function App() {
+const App = () => {
+  const colors = ['red', 'blue', 'green', 'yellow', 'pink', 'black'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ColorPicker colors={colors} />
     </div>
   );
-}
+};
 
 export default App;
